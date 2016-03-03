427c443d2450a733c28d5c3237945963da2a7add
Thomas Powell <t.powell.meto@gmail.com>
2016-03-03 12:05:25 +0000

    let Labby switch on/off aws dev instances in the morning/evening.

--- scripts/cron-jobs.js
+++ scripts/cron-jobs.js
@@ -10,21 +10,21 @@
  *   t.powell.meto@gmail.com
  */
 
-var randItem = function(array){
-  return array[Math.floor(Math.random() * array.length)];
-}
+var randItem = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+};
 
 var CronJob = require('cron').CronJob;
 
 var hellos = [
-  "Morning everyone, I'm in work bright and early and ready to get stuff done!",
-  "Morning all, let's have a productive day!"
-]
+    "Morning everyone, I'm in work bright and early and ready to get stuff done!",
+    "Morning all, let's have a productive day!"
+];
 
 var byes = [
-  "Right, I'm done for the day. Shutting down and going home, Bye!",
-  "Ok I'm off. Shutting down the dev stacks. See ya!"
-]
+    "Right, I'm done for the day. Shutting down and going home, Bye!",
+    "Ok I'm off. Shutting down the dev stacks. See ya!"
+];
 
 /**
  * Hubot command functionality.
@@ -40,6 +40,37 @@ module.exports = function (robot) {
         cronTime: '00 00 08 * * 1-5',
         onTick: function () {
             robot.messageRoom('general', randItem(hellos));
+
+            /*
+             * Start up AWS boxes tagged with an 'environment' of 'dev' by using the global
+             * functionality from the aws-overseer script
+             */
+            var instanceDetails = getEc2Satuses();
+            instanceDetails.then(function (result) {
+                var ids = [];
+                result.forEach(function (instance) {
+                    if (instance.environment === "dev" && instance.state === "stopped") {
+                        ids.push(instance.id);
+                    }
+                });
+                var start = startEc2Instances(ids);
+                start.then(function (result) {
+                    var t = new Table;
+                    result.StartingInstances.forEach(function (instance) {
+                        t.cell("ID", instance.InstanceId);
+                        t.cell("Previous State", instance.PreviousState.Name);
+                        t.cell("Current State", instance.CurrentState.Name);
+                        t.newRow();
+                    });
+                    msg.send("Started AWS Instances:\n" + t.toString());
+                }).catch(function (reason) {
+                    robot.logger.debug(reason, reason.stack);
+                    msg.send("Sorry I was unable to start the dev AWS instances, please check the log file.");
+                });
+            }).catch(function (reason) {
+                robot.logger.debug(reason, reason.stack);
+                msg.send("Sorry I was unable to get the aws instance details, please check the log file.");
+            });
         },
         start: false
     });
@@ -54,6 +85,38 @@ module.exports = function (robot) {
         cronTime: '00 30 17 * * 1-5',
         onTick: function () {
             robot.messageRoom('general', randItem(byes));
+
+            /*
+             * Stops AWS boxes tagged with an 'environment' of 'dev' by using the global
+             * functionality from the aws-overseer script
+             */
+            var instanceDetails = getEc2Satuses();
+            instanceDetails.then(function (result) {
+                var ids = [];
+                result.forEach(function (instance) {
+                    if (instance.environment === "dev" && instance.state === "running") {
+                        ids.push(instance.id);
+                    }
+                });
+                var stop = stopEc2Instances(ids);
+                stop.then(function (result) {
+                    robot.logger.debug(result);
+                    var t = new Table;
+                    result.StoppingInstances.forEach(function (instance) {
+                        t.cell("ID", instance.InstanceId);
+                        t.cell("Previous State", instance.PreviousState.Name);
+                        t.cell("Current State", instance.CurrentState.Name);
+                        t.newRow();
+                    });
+                    msg.send("Stopped AWS Instances:\n" + t.toString());
+                }).catch(function (reason) {
+                    robot.logger.debug(reason, reason.stack);
+                    msg.send("Sorry I was unable to stop the dev AWS instances, please check the log file.");
+                });
+            }).catch(function (reason) {
+                robot.logger.debug(reason, reason.stack);
+                msg.send("Sorry I was unable to get the aws instance details, please check the log file.");
+            });
         },
         start: false
     });
