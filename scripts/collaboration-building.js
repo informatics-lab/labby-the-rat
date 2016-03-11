431b71ad86d9cafc7612adcb73ac6b76ffa24860
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-03-11 14:06:18 +0000

    Posts photo of the collaboration building every week

---
+++ scripts/collaboration-building.js
@@ -0,0 +1,43 @@
+/*
+ * Description:
+ *   Post the live feed of the Collaboration Building
+ *
+ * Dependencies:
+ *   "cron": "^1.0.9"
+ *
+ * Author:
+ *   Jacob Tomlinson
+ *   jacob.tomlinson@informaticslab.co.uk
+ */
+
+var CronJob = require('cron').CronJob;
+
+var sentences = [
+    "Check out the progress on the new building!",
+    "Here's an update on the new building.",
+    "It's coming along!"
+];
+
+/**
+ * Hubot command functionality.
+ */
+module.exports = function (robot) {
+
+    /*
+     * Runs every Friday at 09:30:00 AM. 
+     */
+    var postNewBuilding = new CronJob({
+        cronTime: '00 30 09 * * 5',
+        onTick: function () {
+
+            /*
+             * Give greeting in Slack channel
+             */
+            robot.messageRoom('random', randItem(sentences));
+            robot.messageRoom('random', "https://timelapse.regenology.co.uk/api/latest_image/aOj/");
+        },
+        start: false
+    });
+    postNewBuilding.start();
+
+};
